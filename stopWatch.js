2e2621da2b6fea8e15cdf57e55e0e13e6adc4a5e
OndaTMC <124027519+OndaTMC@users.noreply.github.com>
2023-03-27 15:35:45 +0900

    Update stopWatch.js

--- stopWatch.js
+++ stopWatch.js
@@ -1,70 +1,69 @@
-let [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
-let [overtimeMilliseconds, overtimeSeconds, overtimeMinutes, overtimeHours] = [0, 0, 0, 0];
-let timerRef = document.querySelector('.timerDisplay');
-let overTimeRef = document.querySelector(`.overTimeDisplay`);
-let int = null;
+let [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0]; //各時間の変数を宣言
+let [overtimeMilliseconds, overtimeSeconds, overtimeMinutes, overtimeHours] = [0, 0, 0, 0];//各残業時間の変数を宣言
+let timerRef = document.querySelector('.timerDisplay');//変数に経過時間の表示場所を格納
+let overTimeRef = document.querySelector(`.overTimeDisplay`);//変数に残業の経過時間の表示場所を格納
+let int = null;//変数を初期化
 
-document.getElementById('startTimer').addEventListener('click', () => {
-    if (int !== null) {
+document.getElementById('startTimer').addEventListener('click', () => { //startボタンがクリックされたとき
+    if (int !== null) { //intが初期状態でなければ、インターバルをクリア
         clearInterval(int);
     }
-    int = setInterval(displayTimer, 10);
-    saveData();
-    showTime();
+    int = setInterval(displayTimer, 10); //関数displayTimerをparameter 10でインターバルを実行
+    saveTime(); //localStorageに保存するために関数saveTimeを実行
 });
 
-document.getElementById('pauseTimer').addEventListener('click', () => {
-    clearInterval(int);
-    saveData();
+document.getElementById('pauseTimer').addEventListener('click', () => { //pauseボタンがクリックされたとき
+    clearInterval(int); //インターバルをクリア
+    saveTime();
 });
 
-document.getElementById('resetTimer').addEventListener('click', () => {
-    clearInterval(int);
-    [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
-    timerRef.innerHTML = '00 : 00 : 00';
-    saveData();
+document.getElementById('resetTimer').addEventListener('click', () => { //resetボタンがクリックされたとき
+    clearInterval(int); //インターバルをクリア
+    [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0]; //各時間の変数を0に設定
+    timerRef.innerHTML = '00 : 00 : 00'; //メインのディスプレイに0を表示
+    saveTime();
 });
 
-document.getElementById('overtimeReset').addEventListener('click', () => {
-    clearInterval(int);
-    [overtimeMilliseconds, overtimeSeconds, overtimeMinutes, overtimeHours] = [0, 0, 0, 0];
-    overTimeRef.innerHTML = '00 : 00 : 00';
-    saveData();
+document.getElementById('overtimeReset').addEventListener('click', () => { //overtimeresetボタンがクリックされたとき
+    clearInterval(int); //インターバルをクリア
+    [overtimeMilliseconds, overtimeSeconds, overtimeMinutes, overtimeHours] = [0, 0, 0, 0]; //残業の各時間の変数を0に設定
+    overTimeRef.innerHTML = '00 : 00 : 00'; //overtimeのディスプレイに0を表示
+    saveTime();
 });
 
-function displayTimer() {
-    milliseconds += 10;
+function displayTimer() { //タイマーの関数を宣言
+    milliseconds += 10; //msを10ごとにインクリメント
 
-    if (milliseconds == 1000) {
+    if (milliseconds == 1000) { //1000msで1s
         milliseconds = 0;
         seconds++;
 
-        if (seconds == 60) {
+        if (seconds == 60) { //60sで1min
             seconds = 0;
             minutes++;
 
-            if (minutes == 60) {
+            if (minutes == 60) { //60minで1h
                 minutes = 0;
                 hours++;
             }
         }
     }
 
+    //表示用に新たに変数を宣言
+    let h = hours < 10 ? "0" + hours : hours; //hoursが10以下の場合は、表に2桁目に0を表示
+    let m = minutes < 10 ? "0" + minutes : minutes; //minutesが10以下の場合は、表に2桁目に0を表示
+    let s = seconds < 10 ? "0" + seconds : seconds; //secondsが10以下の場合は、表に2桁目に0を表示
 
-    let h = hours < 10 ? "0" + hours : hours;
-    let m = minutes < 10 ? "0" + minutes : minutes;
-    let s = seconds < 10 ? "0" + seconds : seconds;
 
+    timerRef.innerHTML = ` ${h} : ${m} : ${s}`; //  ディスプレイに経過時間を表示
 
-    timerRef.innerHTML = ` ${h} : ${m} : ${s}`;
-
-    if (h >= 8) {
+    if (s >= 8) { //8時間以上経過した場合はovertimeDisplayTimer関数を実行
         overtimeDisplayTimer();
     }
-    saveData();
+    saveTime();
 }
 
-function overtimeDisplayTimer() {
+function overtimeDisplayTimer() { //overtimeを計測する関数を宣言
     overtimeMilliseconds += 10;
 
     if (overtimeMilliseconds == 1000) {
@@ -89,11 +88,12 @@ function overtimeDisplayTimer() {
 
 
     overTimeRef.innerHTML = ` ${h} : ${m} : ${s}`;
-    saveData();
+    saveTime();
 }
 
 
-function saveData() {
+function saveTime() { //valueをlocalStorageに保存する関数を宣言
+    //各関数のvalueを""内のkeyとセットで格納
     localStorage.setItem("time", timerRef.innerHTML);
     localStorage.setItem("overtime", overTimeRef.innerHTML);
     localStorage.setItem("milliseconds", milliseconds);
@@ -106,7 +106,8 @@ function saveData() {
     localStorage.setItem("overtimeHours", overtimeHours);
 }
 
-function showTime() {
+function showTime() { //valueを指定したkeyによって呼び出す関数を宣言
+    //呼び出したvalueを変数に格納
     const savedTime = localStorage.getItem("time");
     const savedOvertime = localStorage.getItem("overtime");
     const savedMilliseconds = localStorage.getItem("milliseconds");
@@ -118,12 +119,14 @@ function showTime() {
     const savedOverTimeMinutes = localStorage.getItem("overtimeMinutes");
     const savedOverTimeHours = localStorage.getItem("overtimeHours");
 
+    //各変数が初期値でなければ各ディスプレイに表示
     if (savedTime !== null) {
         timerRef.innerHTML = savedTime;
     }
     if (savedOvertime !== null) {
         overTimeRef.innerHTML = savedOvertime;
     }
+    //savedの変数が初期値でなければ各変数にparseIntで数値型に変換して格納
     if (savedMilliseconds !== null) {
         milliseconds = parseInt(savedMilliseconds);
     }
@@ -136,18 +139,19 @@ function showTime() {
     if (savedHours !== null) {
         hours = parseInt(savedHours);
     }
+    //overtimeに関する変数に関しても同様のことを実行
     if (savedOverTimeMilliseconds !== null) {
         overtimeMilliseconds = parseInt(savedOverTimeMilliseconds);
-      }
-      if (savedOverTimeSeconds !== null) {
+    }
+    if (savedOverTimeSeconds !== null) {
         overtimeSeconds = parseInt(savedOverTimeSeconds);
-      }
-      if (savedOverTimeMinutes !== null) {
+    }
+    if (savedOverTimeMinutes !== null) {
         overtimeMinutes = parseInt(savedOverTimeMinutes);
-      }
-      if (savedOverTimeHours !== null) {
+    }
+    if (savedOverTimeHours !== null) {
         overtimeHours = parseInt(savedOverTimeHours);
-      }
+    }
 }
 
 showTime();
